0c7a838bb082b9f198d8723de23fb2e719aa8327
Riccardo Verza <feelthecabbage@gmail.com>
2021-01-14 00:02:18 +0100

    Added methods to call embedded JS functions from Unity.

---
+++ Assets/Scripts/JavascriptFunctionsBridge.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+#if UNITY_WEBGL && !UNITY_EDITOR
+using System.Runtime.InteropServices;
+#endif
+
+public class JavascriptFunctionsBridge : MonoBehaviour
+{
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+    [DllImport("__Internal")]
+    private static extern void HelloString(string str);
+
+    [DllImport("__Internal")]
+    private static extern void PrintFloatArray(float[] array, int size);
+
+    [DllImport("__Internal")]
+    private static extern int AddNumbers(int x, int y);
+
+    [DllImport("__Internal")]
+    private static extern string StringReturnValueFunction();
+
+    [DllImport("__Internal")]
+    private static extern void BindWebGLTexture(int texture);
+
+    void Start()
+    {
+        Hello();
+
+        HelloString("This is a string.");
+
+        float[] myArray = new float[10];
+        PrintFloatArray(myArray, myArray.Length);
+
+        int result = AddNumbers(5, 7);
+        Debug.Log(result);
+
+        Debug.Log(StringReturnValueFunction());
+
+        var texture = new Texture2D(0, 0, TextureFormat.ARGB32, false);
+        BindWebGLTexture(texture.GetNativeTextureID());
+    }
+#endif
+}
